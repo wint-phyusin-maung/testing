1e8ba8b6b25f2cb57ffeeff159016347b6d7ad87
wintphyusinmaung <phyumaung423@gmail.com>
2023-10-27 07:58:26 +0630

    first: commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let sum = 0;
+let newArr = [];
+const generateNumber = (arr) => {
+    for(let i=0; i < arr.length ; i++)
+    {
+        if(arr[i] % 2 == 0) newArr.push(arr[i]);
+    }
+    return newArr;
+}
+
+console.log(generateNumber([2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]));
